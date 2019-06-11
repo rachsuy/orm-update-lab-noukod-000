1138528e6fcfc2a5946dc2f675e3844b4e1c60a6
Rachele Suy <rachelesuy@gmail.com>
2019-06-11 08:07:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,6 +54,26 @@ end
         @id = DB[:conn].execute("Select last_insert_rowid() from students").flatten.first
       end
     end
+def self.create(name,grade)
+        student = Student.new(name,grade)
+        student.save
+        student
+      end
+      
+      def self.new_from_db(row)
+          new_student = self.new(row[0], row[1], row[2])
+          new_student
+        end
+
+        def self.find_by_name(name)
+          sql = <<-SQL
+            select * from students where name = ?
+          SQL
+
+          DB[:conn].execute(sql, name).map do |student|
+            self.new_from_db(student)
+          end.first
+        end
 
 
 end
